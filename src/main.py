3848bbeb6a438ac01344834997cbe951081ce640
Andres Ulloa <andres.ulloa@endava.com>
2021-08-05 16:22:34 -0400

    Configure a Talisman Hook9

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 #!/bin/python
+
+import subprocess
+
 """ Python code for Proof Of Concept"""
 
 print("Hello World")
 
-NUM1 = " "
+NUM1 = 1
 NUM2 = 2
 SUM = NUM1 + NUM2
 print(SUM)
-
-# nslookup.py
-import subprocess
 
 domain = input("Enter the Domain: ")
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding="UTF-8")
