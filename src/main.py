b31708395152b3f2fd13e8fedfb8413590ccc678
Andres Ulloa <andres.ulloa@endava.com>
2021-08-05 15:18:48 -0400

    Configure a Talisman Hook2

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 #!/bin/python
-''' Python code for Proof Of Concept'''
+""" Python code for Proof Of Concept"""
 
 print("Hello World")
 
-NUM1 = 1
+NUM1 = " "
 NUM2 = 2
 SUM = NUM1 + NUM2
 print(SUM)
-
